7f3e64e29a724831c9c71cf1c580b546402dc009
William Adam-Grenier <william.a-g@hotmail.com>
2023-03-31 13:52:15 -0400

    Fix Build Options

--- ModelConverter/cmake/assimp.cmake
+++ ModelConverter/cmake/assimp.cmake
@@ -1,17 +1,19 @@
 include(FetchContent)
 
-set(ASSIMP_WARNINGS_AS_ERRORS OFF)
-set(ASSIMP_BUILD_TESTS OFF)
+set(ASSIMP_WARNINGS_AS_ERRORS OFF CACHE BOOL "Disable warnings as errors" FORCE)
+set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE BOOL "Disable assimp tools" FORCE)
+set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "Disable assimp tests" FORCE)
 
 # No importers will be needed for this project
-set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT OFF)
-set(ASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT OFF)
-set(ASSIMP_BUILD_FBX_EXPORTER ON)
-set(ASSIMP_BUILD_ASSIMP_VIEW OFF)
+set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT OFF CACHE BOOL "Disable all importers" FORCE)
+set(ASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT OFF CACHE BOOL "Disable all exporters" FORCE)
+set(ASSIMP_BUILD_FBX_EXPORTER ON CACHE BOOL "Enable FBX exporter" FORCE)
+set(ASSIMP_BUILD_ASSIMP_VIEW OFF CACHE BOOL "Disable assimp view" FORCE)
 
 FetchContent_Declare(
         assimp
         GIT_REPOSITORY https://github.com/assimp/assimp.git
         GIT_TAG v5.2.5
 )
+
 FetchContent_MakeAvailable(assimp)
